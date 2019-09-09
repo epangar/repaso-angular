c08a90d16b989392e8c3b1cbceaa1149483ffb86
Estanislao Pan García <epan@indra.es>
2019-09-09 16:35:22 +0200

    Chart

--- src/app/chart/chart.component.ts
+++ src/app/chart/chart.component.ts
@@ -13,14 +13,19 @@ import { Chart, ChartData, Point, ChartColor } from 'chart.js';
 
 export class ChartComponent implements OnInit {
 
-  algorithm:string
+  algorithm:string =""
   totalSeats:number = 0;
-  
+  totalValidVotes:number=0;
+  minimum:number=0;
+  partiesWithSeats:Partyvotes[];
+  finalComposition:Partyvotes[];
+
   currentParty: Partyvotes = {
     party:"",
     votes:"",
   }
   finalResults: Partyvotes[] =[]
+  
   
 
   constructor() { 
@@ -38,13 +43,17 @@ export class ChartComponent implements OnInit {
     this.currentParty = {
       party:"",
       votes:"",
-    }
+    };
+    (this.getValidVotes(this.finalResults));
+    this.whoGetsSeats(this.finalResults, this.minimum);
     return  
   }
 
   remove(index: number){
     this.finalResults=this.finalResults
-                              .filter((object)=>this.finalResults.indexOf(object)!==index)
+                              .filter((object)=>this.finalResults.indexOf(object)!==index);
+                              debugger
+    (this.getValidVotes(this.finalResults));
   }
 
   submitForm(myForm: NgForm) {
@@ -53,11 +62,51 @@ export class ChartComponent implements OnInit {
     console.log(NgModel)
     this.finalResults=[];
     this.finalResults=myForm.value;
-    console.log(this.finalResults)
-    
-    
+    console.log(this.finalResults);
+    this.getValidVotes(this.finalResults);
+    this.getComposition(this.partiesWithSeats, this.algorithm, this.totalSeats)
 }
 
+  getValidVotes(finalResults):void{
+    if(finalResults.length===0){
+      this.totalValidVotes=0;
+    }else {
+      this.totalValidVotes= finalResults.map(party=>party.votes).reduce((a,b)=>a+b)
+    }
+    
+  };
+
+  whoGetsSeats(finalResults: Partyvotes[], minimum: number){
+    this.partiesWithSeats= finalResults.filter(party=>party['votes'] >= (this.totalValidVotes/100)*minimum)
+  }
+  
+  getComposition(partiesWithSeats:Partyvotes[], algorithm:string, seats:number ){
+
+    let divisor:number=1, simulatedArray:Partyvotes[]=[], dividedVotes:Partyvotes[]=[];
+
+    while(this.finalComposition.length < seats){
+        debugger
+        dividedVotes=this.partiesWithSeats.map(party=>{
+                                                        party['votes']/=divisor;
+                                                        return party;
+                                                        })
+        dividedVotes.forEach(party=>{
+            simulatedArray.push(party);
+            simulatedArray.sort((a,b)=>a['votes']-b['votes'])  
+        })
+
+        if(algorithm==="D'Hont"){
+            divisor++
+        } else if(algorithm==="Saint-League"){
+            divisor+=2
+        }
+
+        
+    }
+    
+
+
+}
   generateCanvas(){
     
    
--- src/classes/Parliament.ts
+++ src/classes/Parliament.ts
@@ -2,49 +2,12 @@ import { Partyvotes } from './Partyvotes'
 
 class Parliament {
     constructor(results: Partyvotes[], algorithm: string, minimum: number, seats:number){
+        results=results;
+        algorithm=algorithm;
+        minimum=minimum;
+        seats=seats;
         
-        
     }
-    totalValidVotes:number=0;
-    partiesWithSeats:Partyvotes[];
-    finalComposition:Partyvotes[];
-    
-    getValidVotes(results):void{
-        this.totalValidVotes= results.map(party=>party.votes).reduce((a,b)=>a+b)
-    };
-
-    whoGetsSeats(results: Partyvotes[], minimum: number){
-        this.partiesWithSeats= results.filter(party=>party['votes'] >= (this.totalValidVotes/100)*minimum)
-    }
-
-    getComposition(partiesWithSeats:Partyvotes[], algorithm:string, seats:number ){
-
-        let divisor:number=1, simulatedArray:Partyvotes[]=[], dividedVotes:Partyvotes[]=[];
-
-        while(this.finalComposition.length < seats){
-            
-            dividedVotes=this.partiesWithSeats.map(party=>{
-                                                            party['votes']/=divisor;
-                                                            return party;
-                                                            })
-            dividedVotes.forEach(party=>{
-                simulatedArray.push(party);
-                simulatedArray.sort((a,b)=>a['votes']-b['votes'])  
-            })
-
-            if(algorithm==="D'Hont"){
-                divisor++
-            } else if(algorithm==="Saint-League"){
-                divisor+=2
-            }
-
-            
-        }
-
-
-    }
-
-
 
 }
 
